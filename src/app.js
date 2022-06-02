3ed55c5dde979e6cc23eca9f6e61473abf8db408
George <cradplay@gmail.com>
2022-06-02 02:09:16 -0400

    output menu added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
-const message = "nazari_lox";
+var expressionBuffer = "";
+var inputField = document.getElementById('calcInput');
+var resultHolder = document.getElementById('result'); //previous result
 
-export { message };
+var bHistoryOpen = false;
+
+resultHolder.innerHTML = '532';
+
+inputField.addEventListener('input', () => {
+    expressionBuffer = inputField.value;
+    console.log(inputField.value);
+});
+
+
+function addToCalcBuffer(symbol){
+    expressionBuffer += symbol;
+    inputField.value = expressionBuffer;
+
+    console.log("added " + inputField.value);
+}
+
+function openHistory (){
+
+}
+
+function calculate(input){
+    let output;
+
+    resultHolder.innerHTML = output;
+}
